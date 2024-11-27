e74876b7c5739d14593480534966d9641256d282
Elvis-785 <kiruielvis3358@gmail.com>
2024-11-28 00:48:51 +0300

    props in components

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,16 @@
 import React from "react";
+import user from "../data/user"
+import Links from "./Links";
+function About({ bio, github, linkedin }) { 
+  return ( 
+    <div id="about"> 
+      <h2>About Me</h2> 
+      {bio && <p>{bio}</p>}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" /> 
+      <Links github={github} linkedin={linkedin} /> 
+    </div> 
+    ); 
+  } 
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
-}
 
 export default About;
